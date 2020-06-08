2cac25c60e1f3219d031ab86789401d038457ec9
Korman Chen <korchen@ucdavis.edu>
2020-06-08 01:02:23 -0700

    Updates

---
+++ precache-manifest.586f01464262955af5868674b8ed0a1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc2f95d16ef598d604fedb2afe86372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b2b34c2dbbf587309e2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b88678da58cda4de9179",
+    "url": "/static/js/2.102137c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.102137c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2b34c2dbbf587309e2",
+    "url": "/static/js/main.0e58917e.chunk.js"
+  },
+  {
+    "revision": "7b0e46e84d161416da5b",
+    "url": "/static/js/runtime-main.91f38002.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
